a94c6cc163d393e26c4047a1b9d856e887d16aa0
f06102229 - Marcos Turisco <marcos.turisco@sefin.fortaleza.gov.br>
2026-02-04 19:41:52 -0300

    configurando sistema para rodar em application load balancer

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 ./build.sh
-aws ecs update-service --cluster cluster-bia --service service-bia --force-new-deployment
+aws ecs update-service --cluster cluster-bia-alb --service service-bia-alb --force-new-deployment
 
